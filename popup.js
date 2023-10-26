5a51863805522c007cd7bb7da5c86483032e1a08
valentina buoro <valentinabuoro4@gmail.com>
2023-10-26 01:38:48 +0100

    fix: removed unwanted comments

--- popup.js
+++ popup.js
@@ -55843,48 +55843,9 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   function scrapeEmailsFromPage() {
-    //alert("hello");
     const emailRegex = /[\w\.=-]+@[\w\.-]+\.[\w]{2,3}/gim;
-    //parse the page for emails
-    //const emailRegex = /(?:https?|ftp):\/\/[^\s/$.?#].[^\s]*|www\.[^\s/$.?#].[^\s]/gim;
-    //const emailRegex = /(?:https?|ftp):\/\/[^\s/$.?#].[^\s]*|www\.[^\s/$.?#].[^\s]*/;
     let emails = document.documentElement.innerText.match(emailRegex);
-    //alert(emails);
-
-    //send the emails to popup
     chrome.runtime.sendMessage({ emails });
   }
-
-  // anything else you want to initialize on the page
-  console.log("held");
 });
 
-/*document.addEventListener('DOMContentLoaded', function() {
-    
-    document.querySelector('button').addEventListener('click', 
-    onclick, false)
-
-    function onclick(){
-        chrome.tabs.query({currentWindow: true, active: true},
-        function (tabs){
-            chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
-        })
-    }
-    
-    function setCount (res) {
-        if (res.count > 6) {
-            const div = document.createElement('div')
-            div.textContent = 'This is a spam/phising email!'
-            div.style = 'color: red; font-size: 18px;'
-            document.body.appendChild(div)
-        }
-        else {
-            const div = document.createElement('div')
-            div.textContent = 'This is not a spam/phising email'
-            div.style = 'color: green; font-size: 18px;'
-            document.body.appendChild(div)
-        }
-        const br = document.createElement('br')
-        document.body.appendChild(br)
-    }
-}, false)*/
